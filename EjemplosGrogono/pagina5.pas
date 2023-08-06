08beed95b8626022861849d5f895e4177ee6e936
Juan Federico <juanfedericomartinez@gmail.com>
2023-08-06 17:00:39 -0300

    Update pagina5.pas
    { Copyright (C) 2023 by Juan Federico Martinez <mail@federicomartinez.com.ar> Ejemplo Página 5 - (005.133 PASCAL G874p) SQUAREROOTOFTWO - Cálculo de la raíz cuadrada de el número 2 //   This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>. }
    
    PROGRAM RaizCuadradaDeDos (output); { Nombre del Programa y su output }
        BEGIN { Inicio del programa}
            write (sqrt(2)) {salida compilada 1.41421356237309504876E+0000j }
        END. { FIN del programa}

--- EjemplosGrogono/pagina5.pas
+++ EjemplosGrogono/pagina5.pas
@@ -1,1 +1,6 @@
+{ Copyright (C) 2023 by Juan Federico Martinez <mail@federicomartinez.com.ar> Ejemplo Página 5 - (005.133 PASCAL G874p) SQUAREROOTOFTWO - Cálculo de la raíz cuadrada de el número 2 //   This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>. }
 
+PROGRAM RaizCuadradaDeDos (output); { Nombre del Programa y su output }
+    BEGIN { Inicio del programa}
+        write (sqrt(2)) {salida compilada 1.41421356237309504876E+0000j }
+    END. { FIN del programa}
